ebe9050797234f22d538021882796c7bfad33c4c
Ridwan Romadhon <ridwanromadhon48@gmail.com>
2020-09-22 16:56:49 +0700

    perbaikan Router

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::view('home', 'home')->middleware('auth');
